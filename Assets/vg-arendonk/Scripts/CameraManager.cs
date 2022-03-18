7639e19df9a879becbc49590b3fe65356ca7b0cd
Douwe Ravers <douwe.ravers@hotmail.com>
2022-03-18 23:41:35 +0100

    Created a 3D model of the Arendonkse church. Decided on using the old UI system.

---
+++ Assets/vg-arendonk/Scripts/CameraManager.cs
@@ -0,0 +1,37 @@
+using Cinemachine;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    void Start()
+    {
+        CinemachineCore.GetInputAxis = GetAxisCustom;
+    }
+
+    public float GetAxisCustom(string axisName)
+    {
+        if (axisName == "Mouse X")
+        {
+            if (Input.GetMouseButton(0))
+            {
+                return Input.GetAxis("Mouse X");
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        else if (axisName == "Mouse Y")
+        {
+            if (Input.GetMouseButton(0))
+            {
+                return Input.GetAxis("Mouse Y");
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        return Input.GetAxis(axisName);
+    }
+}
