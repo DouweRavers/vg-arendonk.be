e543d8bbff2893f75544795b91428a08e4c6c597
Douwe Ravers <douwe.ravers@hotmail.com>
2022-03-20 18:37:37 +0100

    Menu submenu system. Start development.

---
+++ Assets/vg-arendonk/Scripts/MenuItem.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+[RequireComponent(typeof(Text))]
+[RequireComponent(typeof(Button))]
+public class MenuItem : MonoBehaviour
+{
+    public bool hasSubmenu = false;
+}
--- Assets/vg-arendonk/Scripts/MenuManager.cs
+++ Assets/vg-arendonk/Scripts/MenuManager.cs
@@ -1,26 +1,69 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class MenuManager : MonoBehaviour
 {
-    [SerializeField] float _scrollScaler = 0.05f;
+    [SerializeField] float ScrollScaler = 0.05f;
+
+    public float ScrollValue { get { return _scrollValue; } }
     float _scrollValue = 0f;
+
+    MenuItem[] Pages;
+
+
+
+    void Start()
+    {
+        ScanForPages();
+        PositionPages();
+    }
+
+
 
     void Update()
     {
-        _scrollValue = Mathf.Clamp(_scrollValue - Input.mouseScrollDelta.y * _scrollScaler, 0, 0.99f);
-        Text[] items = GetComponentsInChildren<Text>();
-        for (int i = 0; i < items.Length; i++)
+        _scrollValue = Mathf.Clamp(_scrollValue - Input.mouseScrollDelta.y * ScrollScaler, 0, 1);
+        if (Input.mouseScrollDelta.y != 0)
         {
-            items[i].fontSize = 0.25f * i <= _scrollValue && _scrollValue < 0.25f * (i+1) ? 80 : 50;
-            items[i].rectTransform.anchoredPosition = new Vector2(
-                300 * Mathf.Cos(Mathf.Deg2Rad * Mathf.Lerp(0 - 15 * i, 45 - 15 * i, _scrollValue)),
-                300 * Mathf.Sin(Mathf.Deg2Rad * Mathf.Lerp(0-15*i, 45 - 15 * i, _scrollValue))
-                );
+            PositionPages();
         }
-        Debug.Log(_scrollValue);
+    }
+    void ScanForPages()
+    {
+        List<MenuItem> PageItems = new List<MenuItem>();
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            MenuItem item;
+            if (transform.GetChild(i).TryGetComponent<MenuItem>(out item)) PageItems.Add(item);
+        }
+        Pages = PageItems.ToArray();
+    }
+
+    void PositionPages()
+    {
+        float additionalAngle = 0;
+        for (int i = 0; i < Pages.Length; i++)
+        {
+            float angle = Mathf.Lerp(0 - 15 * i, 15 * (Pages.Length - 1) - 15 * i, _scrollValue);
+            Pages[i].GetComponent<RectTransform>().anchoredPosition = new Vector2(
+                300 * Mathf.Cos(Mathf.Deg2Rad * (angle + additionalAngle)),
+                300 * Mathf.Sin(Mathf.Deg2Rad * (angle + additionalAngle))
+                ); 
+            if (0 <= angle && angle < 15)
+            {
+                Pages[i].GetComponent<Text>().fontSize = 80;
+                if (Pages[i].hasSubmenu) {
+                    additionalAngle = -20;
+                }
+            }
+            else {
+                Pages[i].GetComponent<Text>().fontSize = 50;
+
+            }
+            
+        }
     }
 
 }
+
